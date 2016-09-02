82aecec0ce4e98602e5fc76e6d9e98310f9963bc
Travis Nesland <tnesland@gmail.com>
2016-09-02 08:54:21 -0400

    Fetch data and render table.

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,21 @@
 angular.module('roster', [])
-  .controller('tableCtrl', function($scope) {
-    $scope.msg = "foo foo foo";
+
+  /* Handle the concerns of the player table */
+  .controller('tableCtrl', function($scope, playersService) {
+
+    /* Get player data from the service  */
+    playersService.getPlayers().then(
+      function(response) { $scope.players = response.data; console.log($scope.players[0]); },
+      function(err) { console.error(err); });
+  })
+
+  /* Handle data layer for players - in this case this service is
+  extremely minimal, simply returning promises for any endpoints, but
+  I still like to keep the controller decoupled from the actual API */
+  .service('playersService', function($http) {
+
+    /* Return a promise that resolves with player data */
+    this.getPlayers = function(onSuc, onErr) {
+      return $http.get('/players');
+    }
   });

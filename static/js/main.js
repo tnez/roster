1a38865700d202f12972084950edb283eadf6cd5
Travis Nesland <tnesland@gmail.com>
2016-09-02 13:50:45 -0400

    Hook up functionality to select player from list.

--- static/js/main.js
+++ static/js/main.js
@@ -8,23 +8,68 @@ angular.module('roster', [])
       battingAvg: "-",
       hr: "-",
       rbi: "-"
-    }
+    };
+
+    /* when currentPlayer has changed on the playerService, update our
+    local scope */
+    $scope.$watch(function() { return playersService.currentPlayerId; },
+      function(newValue, oldValue) {
+        console.log("newValue:", newValue, "oldValue:", oldValue);
+        $scope.currentPlayer = playersService.getPlayer(newValue);
+      }
+    );
   })
 
   /* Handle the concerns of the player table */
   .controller('tableCtrl', function($scope, playersService) {
 
-    /* Get player data from the service  */
-    playersService.getPlayers().then(
-      function(response) { $scope.players = response.data; console.log($scope.players[0]); },
-      function(err) { console.error(err); });
+    /* Get player data from the service and set $scope.players */
+    playersService.getPlayers(function(data) { $scope.players = data; });
+
+    /* Provide a function to select a given player making that player
+       the active player in the playersService */
+    $scope.selectPlayer = function(playerId) {
+      playersService.currentPlayerId = playerId;
+    };
   })
 
   /* Manage player data */
   .service('playersService', function($http) {
+    s = this
+    s.currentPlayerId = null;
+    s.players = [];
 
     /* Return a promise that resolves with player data */
-    this.getPlayers = function(onSuc, onErr) {
-      return $http.get('/players');
+    s.getPlayers = function(cb) {
+      $http.get('/players').then(
+        function(response) {
+          transformed_data = response.data.map(s.transformPlayerData);
+          s.players = transformed_data;
+          cb(s.players);
+        },
+        function(err) { console.error(err); }
+      )
+    };
+
+    /* Return a player by ID */
+    s.getPlayer = function(id) {
+      return s.players[id];
+    };
+
+    /* Given raw player data, transform it to something more
+       digestible for our controllers - here we are also faking some
+       of our data ... we don't have baseball specific attributes from
+       our fake data resource! */
+    s.transformPlayerData = function(orig) {
+      return {
+        battingAvg: (0.275 + (Math.random() - 0.5) / 10.0).toFixed(3), // looks weird, but should generate .275 +/- .100 which seems reasonable
+        hr: Math.round(15 + (Math.random() - 0.5) * 10),
+        rbi: Math.round(50 + (Math.random() - 0.5) * 40),
+        dob: orig.dob.split(' ')[0],
+        email: orig.email,
+        name: [orig.name.first, orig.name.last].join(' '),
+        phone: orig.phone,
+        photo: orig.picture.large
+      };
     }
-  });
+  })
